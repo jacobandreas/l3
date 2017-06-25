93cc231dd5e6356c6b085dad1d22e31c18680ce3
Jacob Andreas <jda@cs.berkeley.edu>
2017-06-25 09:11:57 -0700

    exp

---
+++ exp/pbd_hint/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+python -u ../../pbd.py \
+  --hint_type=nl \
+  --predict_hyp=true \
+  --infer_hyp=true \
+  --n_sample_hyps=10 \
+  --learning_rate 0.001 \
+  --train \
+  --n_epochs=750 \
+  --test \
+  > train.out \
+  2> train.err
+
