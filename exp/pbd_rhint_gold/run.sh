36a2019e8c28bb9908e23e95a45a1e52da978924
Jacob Andreas <jda@tomato.banatao.berkeley.edu>
2017-06-29 13:44:11 -0700

    new exps

---
+++ exp/pbd_rhint_gold/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+python -u ../../pbd.py \
+  --hint_type=re \
+  --predict_hyp=true \
+  --infer_hyp=true \
+  --use_true_hyp=true \
+  --n_sample_hyps=10 \
+  --learning_rate 0.001 \
+  --train \
+  --n_epochs=750 \
+  --test \
+  > train.out \
+  2> train.err
+
