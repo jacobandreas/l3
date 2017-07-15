643f610cbfdab5b4a687b55fbd50a229ab8e082e
Jacob Andreas <jda@tomato.banatao.berkeley.edu>
2017-07-14 20:48:15 -0700

    .

--- exp/cls_ex/run.sh
+++ exp/cls_ex/run.sh
@@ -3,12 +3,12 @@
 export PYTHONPATH=".:../../../shapeworld"
 
 python -u ../../cls.py \
-  --predict_hyp=false \
-  --infer_hyp=false \
+  --predict_hyp=true \
+  --infer_hyp=true \
   --learning_rate 0.001 \
   --train \
   --n_epochs=100 \
   --test \
-  > train.out \
-  2> train.err
+  #> train.out \
+  #2> train.err
 
