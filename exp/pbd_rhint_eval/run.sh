008812b049c0318d91d0c06c586f7c3dda5a939a
Jacob Andreas <jda@cs.berkeley.edu>
2017-09-05 23:53:28 -0700

    regices

--- exp/pbd_rhint_eval/run.sh
+++ exp/pbd_rhint_eval/run.sh
@@ -6,7 +6,7 @@ python -u ../../pbd.py \
   --infer_hyp=true \
   --infer_by_likelihood=true \
   --n_sample_hyps=100 \
-  --restore="../pbd_rhint/model" \
+  --restore="../pbd_rhint/model.chk" \
   --test \
   > eval.out \
   2> eval.err
